823fab839cbbd96c077d41155734fcb20b333d7d
TarunMahawar <tarunmahawar25@gmail.com>
2017-03-24 07:42:36 +0530

    Upload post functionality compleated

--- app/src/main/java/in/coders/fsociety/taskbuddy/Utils/ApiInterface.java
+++ app/src/main/java/in/coders/fsociety/taskbuddy/Utils/ApiInterface.java
@@ -1,4 +1,20 @@
 package in.coders.fsociety.taskbuddy.Utils;
 
-public class ApiInterface {
+import com.facebook.login.LoginFragment;
+
+import in.coders.fsociety.taskbuddy.UploadPost;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface ApiInterface {
+
+    @POST("/post")
+    @FormUrlEncoded
+    Call<UploadPost.Response1> sendPostData(@Field("title") String title, @Field("description") String description,
+                                            @Field("credit") int credit
+            , @Field("tags") String tag, @Field("picPostUrl") String picPostUrl,@Field("isPublic") String isPublic  ,@Field("authorId") String authorid ,
+                                            @Field("circlesId") String circlesId);
+
 }
